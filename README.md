7889ad0fa6d8e057c8cec67b92c686e3d307c888
minsang <minsang@minsang-macbookair.roam.corp.google.com>
2015-08-10 00:10:54 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tumblr-Prototype
---
+++ testproto/home.swift
@@ -0,0 +1,35 @@
+//
+//  home.swift
+//  testproto
+//
+//  Created by minsang on 7/26/15.
+//  Copyright (c) 2015 minsang. All rights reserved.
+//
+
+import UIKit
+
+class home: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
